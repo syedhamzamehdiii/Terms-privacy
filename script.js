37c92a2cfb7435b0c809f11013798ac2ee13e9f0
Syed Hamza Mehdi <syedhm@Syeds-MacBook-Air.local>
2026-05-22 23:30:06 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuBtn = document.getElementById('menuBtn');
+const navLinks = document.getElementById('navLinks');
+if (menuBtn && navLinks) {
+  menuBtn.addEventListener('click', () => navLinks.classList.toggle('open'));
+  navLinks.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => navLinks.classList.remove('open'));
+  });
+}
